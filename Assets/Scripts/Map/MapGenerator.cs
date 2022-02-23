f457c4568321452daa3ba3e0f9703e2ffdbf44d8
MORTX007 <mortx007@gmail.com>
2022-02-23 13:32:25 -0500

    No Endless Terrain

--- Assets/Scripts/Map/MapGenerator.cs
+++ Assets/Scripts/Map/MapGenerator.cs
@@ -35,9 +35,6 @@ public class MapGenerator : MonoBehaviour
 
 	float[,] falloffMap;
 
-	Queue<MapThreadInfo<MapData>> mapDataThreadInfoQueue = new Queue<MapThreadInfo<MapData>>();
-	Queue<MapThreadInfo<MeshData>> meshDataThreadInfoQueue = new Queue<MapThreadInfo<MeshData>>();
-
     private void Awake()
     {
 		falloffMap = FalloffGenerator.GenerateFalloffMap(mapChunkSize);
@@ -63,65 +60,6 @@ public class MapGenerator : MonoBehaviour
         {
 			display.DrawTexture(TextureGenerator.TextureFromHeightMap(FalloffGenerator.GenerateFalloffMap(mapChunkSize)));
         }
-	}
-
-	public void RequestMapData(Vector2 centre, Action<MapData> callback)
-    {
-		ThreadStart threadStart = delegate
-		{
-			MapDataThread(centre, callback);
-		};
-
-		new Thread(threadStart).Start();
-    }
-
-	void MapDataThread(Vector2 centre, Action<MapData> callback)
-    {
-		MapData mapData = GenerateMapData(centre);
-		lock (mapDataThreadInfoQueue)
-		{
-			mapDataThreadInfoQueue.Enqueue(new MapThreadInfo<MapData>(callback, mapData));
-		}
-    }
-
-	public void RequestMeshData(MapData mapData, int lod, Action<MeshData> callback)
-    {
-		ThreadStart threadStart = delegate
-		{
-			MeshDataThread(mapData, lod, callback);
-		};
-
-		new Thread(threadStart).Start();
-	}
-
-	void MeshDataThread(MapData mapData, int lod, Action<MeshData> callback)
-    {
-		MeshData meshData = MeshGenerator.GenerateTerrainMesh(mapData.heightMap, meshHeightMultiplier, meshHeightCurve, lod);
-		lock (meshDataThreadInfoQueue)
-        {
-			meshDataThreadInfoQueue.Enqueue(new MapThreadInfo<MeshData>(callback, meshData));
-        }
-    }
-
-    private void Update()
-    {
-		if (mapDataThreadInfoQueue.Count > 0)
-		{
-			for (int i = 0; i < mapDataThreadInfoQueue.Count; i++)
-			{
-				MapThreadInfo<MapData> threadInfo = mapDataThreadInfoQueue.Dequeue();
-				threadInfo.callback(threadInfo.parameter);
-			}
-		}
-
-		if (meshDataThreadInfoQueue.Count > 0)
-		{
-			for (int i = 0; i < meshDataThreadInfoQueue.Count; i++)
-			{
-				MapThreadInfo<MeshData> threadInfo = meshDataThreadInfoQueue.Dequeue();
-				threadInfo.callback(threadInfo.parameter);
-			}
-		}
 	}
 
     MapData GenerateMapData(Vector2 centre)
@@ -168,19 +106,6 @@ public class MapGenerator : MonoBehaviour
 
 		falloffMap = FalloffGenerator.GenerateFalloffMap(mapChunkSize);
 	}
-
-	struct MapThreadInfo<T>
-    {
-		public readonly Action<T> callback;
-		public readonly T parameter;
-
-		public MapThreadInfo(Action<T> callback, T parameter)
-        {
-			this.callback = callback;
-			this.parameter = parameter;
-        }
-
-    }
 }
 
 [System.Serializable]
